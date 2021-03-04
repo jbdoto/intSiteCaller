bb6ab30f2547f53107f8ef29a025ad2bf86c1943
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-04 11:02:51 -0500

    Login during build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+aws ecr get-login-password --region us-east-1 --profile=jdoto-ab3 | docker login --username AWS --password-stdin 483158796244.dkr.ecr.us-east-1.amazonaws.com
 docker build . -t intsitecaller
 docker build -f entrypoint.dockerfile . --build-arg BASE_IMAGE=intsitecaller -t intsitecaller
 docker tag intsitecaller 483158796244.dkr.ecr.us-east-1.amazonaws.com/intsitecaller:1.0.0

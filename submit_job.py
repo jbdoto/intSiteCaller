3df2d8a8836e614bc2cd3a61a82a158942e117d2
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-05 01:05:27 -0500

    Fix position of cpu, mem args in submit_job.py

--- submit_job.py
+++ submit_job.py
@@ -17,22 +17,25 @@ parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFo
 parser.add_argument("--vcpus", help="overrides the configured vcpus for the job", type=str, default='1')
 parser.add_argument("--memory", help="overrides the configured memory for the job", type=str, default='8000')
 parser.add_argument("--job-name", help="name of the job", type=str, default='intsitecaller')
-parser.add_argument("--job-queue", help="name of the job queue to submit this job", type=str, default='intsitecaller-job-queue')
+parser.add_argument("--job-queue", help="name of the job queue to submit this job", type=str,
+                    default='intsitecaller-job-queue')
 parser.add_argument("--job-definition", help="name of the job job definition", type=str, default="intsitecaller")
 parser.add_argument("--run-command", help="command to run", type=str)
 parser.add_argument("--bucket-name", help="the name of the source data bucket, eg: intsitecaller-samples", type=str)
 parser.add_argument("--object-name", help="the name of the source data tar.gz file, eg: G6RV5.tar.gz", type=str)
 parser.add_argument("--sample-id", help="the sample id, eg: G6RV5", type=str)
-parser.add_argument("--serial-wait", help="whether the subsequent job should submit commands to the os and wait. ",dest='feature', action='store_true', default=False)
+parser.add_argument("--serial-wait", help="whether the subsequent job should submit commands to the os and wait. ",
+                    dest='feature', action='store_true', default=False)
 parser.add_argument("--job-type", help="whether this is a parent or child job. ", type=str)
-parser.add_argument("--parent-aws-batch-job-id", help="the job id of the parent batch job that started this job", type=str)
-parser.add_argument("--parent-aws-batch-job-attempt", help="the job attempt count of the parent batch job that started this job", type=str)
+parser.add_argument("--parent-aws-batch-job-id", help="the job id of the parent batch job that started this job",
+                    type=str)
+parser.add_argument("--parent-aws-batch-job-attempt",
+                    help="the job attempt count of the parent batch job that started this job", type=str)
 
 args = parser.parse_args()
 
 
 def main():
-
     job_name = args.job_name
     job_queue = args.job_queue
     job_definition = args.job_definition
@@ -43,18 +46,16 @@ def main():
     job_type = args.job_type
     parent_aws_batch_job_id = args.parent_aws_batch_job_id
     parent_aws_batch_job_attempt = args.parent_aws_batch_job_attempt
-    vcpus = args.vcpus
-    memory = args.memory
+    vcpus = int(args.vcpus)
+    memory = int(args.memory)
     print('got args: %s', args)
 
-    submitJobResponse = batch.submit_job(
+    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/batch.html#Batch.Client.submit_job
+    submit_job_response = batch.submit_job(
         jobName=job_name,
         jobQueue=job_queue,
         jobDefinition=job_definition,
-        # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/batch.html#Batch.Client.submit_job
-        containerOverrides={'environment': [{"name": "vcpus", "value": vcpus},
-                                            {"name": "memory", "value": memory},
-                                            {"name": "RUN_COMMAND", "value": run_command},
+        containerOverrides={'environment': [{"name": "RUN_COMMAND", "value": run_command},
                                             {"name": "BUCKET_NAME", "value": bucket_name},
                                             {"name": "OBJECT_NAME", "value": object_name},
                                             {"name": "SAMPLE_ID", "value": sample_id},
@@ -64,12 +65,15 @@ def main():
                                             {"name": "JOB_QUEUE", "value": job_queue},
                                             {"name": "JOB_DEFINITION", "value": job_definition},
                                             {"name": "PARENT_AWS_BATCH_JOB_ATTEMPT",
-                                             "value": parent_aws_batch_job_attempt}]}
+                                             "value": parent_aws_batch_job_attempt}],
+                            "vcpus": vcpus,
+                            "memory": memory
+                            }
     )
 
-    jobId = submitJobResponse['jobId']
+    jobId = submit_job_response['jobId']
     print('Submitted job [%s - %s] to the job queue [%s]' % (job_name, jobId, job_queue))
+
 
 if __name__ == "__main__":
     main()
-

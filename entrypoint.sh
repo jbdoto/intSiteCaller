0337f90c12c68c6ddedefd036e351ee20e17c433
Jeffrey Doto <jeffdoto@amazon.com>
2021-03-09 12:19:16 -0500

    entrypoint.sh now captures job duration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,20 @@
 #!/bin/bash -e
 
+put_metric_data() {
+
+  # compute time elapsed
+  end_time=$(date +%s%3N)
+  time_delta=$(($end_time - ${1}))
+
+  echo "Job took ${time_delta} ms"
+  aws cloudwatch put-metric-data --metric-name JobDuration --namespace intSiteCaller --unit Milliseconds --value ${time_delta} --dimensions JobName=${JOB_NAME} --region ${AWS_REGION}
+}
+
+trap 'put_metric_data ${start_time}' exit
+
+start_time=$(date +%s%3N)
+echo "Starting ${JOB_NAME}..."
+
 if [[ ${JOB_TYPE} == 'CHILD' ]]
 then
   cd /scratch/results/${PARENT_AWS_BATCH_JOB_ID}/${PARENT_AWS_BATCH_JOB_ATTEMPT};
